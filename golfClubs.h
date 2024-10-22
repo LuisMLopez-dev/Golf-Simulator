913c80f54cea2b5ac6bb2baef478bc022617045d
Luis M. Lopez <luismlopez2004@gmail.com>
2024-10-22 15:46:43 -0400

    Add files via upload

---
+++ golfClubs.h
@@ -0,0 +1,343 @@
+//Header Files
+#include <stdio.h>
+#include <stdlib.h>
+
+//Prints the info for the clubs
+void clubsInfo(void){
+    const char *clubs[] = {
+        "0 --> Putter(max. 25yd)", 
+        "1 --> Wedge(max. 35yd)", 
+        "2 --> 9 Iron(max. 71yd)", 
+        "3 --> 7 Iron(max. 108yd)", 
+        "4 --> 5 Iron(max. 145yd)", 
+        "5 --> 3 Iron(max. 182yd)", 
+        "6 --> 3 Wood(max. 219yd)", 
+        "7 --> Driver(max. 255yd)"
+    };
+
+    int numClubs = sizeof(clubs) / sizeof(clubs[0]);
+    for(int i = 0; i < numClubs; i++){
+        printf("%s\n", clubs[i]);
+    }
+}
+
+//Prints the info for the holes
+void holesInfo(void){
+        const char *holes[] = {
+        "HOLE INFORMATION:", 
+        "Hole One: Hole Location: 300 yards Par: 3",
+        "Hole Two: Hole Location: 500 yards Par: 4", 
+        "Hole Three: Hole Location: 200 yards Par: 3", 
+        "Hole Four: Hole Location: 700 yards Par: 5"
+    };
+
+    int numHoles = sizeof(holes) / sizeof(holes[0]);
+    for(int i = 0; i < numHoles; i++){
+        printf("%s\n", holes[i]);
+    }
+}
+
+/* Comments for the functions below:
+1. The following functions below use randomized seeds to obtain the swing values.
+2. Each function is for a specific club, and each club has a range for the swing values.
+3. These clubs have strength settings to split the total range, into smaller subranges for the swing values.
+4. In total there are 8 clubs, and the total range is between 1-255 yards.
+*/
+
+int putter(void){
+    int userNumber;
+    int swingValue;
+    const char *putter[] = {
+        "Specify the strength of your swing:\n",
+        "1 --> Very Light (1 - 2yd)\n",
+        "2 --> Light (3 - 5yd)\n",
+        "3 -- > Medium (6 - 10yd)\n",
+        "4 --> Hard (11 - 16yd)\n",
+        "5 --> Very Hard (17 - 25yd)\n\n"
+    };
+
+    int putterStrength = sizeof(putter) / sizeof(putter[0]);
+    for(int i = 0; i < putterStrength; i++){
+        printf("%s\n", putter[i]);
+    }
+
+    do{
+        printf("\nEnter one of the numbers listed\nYour number: ");
+        scanf("%d", &userNumber);
+    }while(userNumber < 1 || userNumber > 5);
+
+    switch(userNumber){
+        case 1:
+            swingValue = 1 + (rand() % 2);
+            break;
+
+        case 2:
+            swingValue = 3 + (rand() % 3);
+            break;
+
+        case 3:
+            swingValue = 6 + (rand() % 5);
+            break;
+
+        case 4:
+            swingValue = 11 + (rand() % 6);
+            break;
+
+        case 5:
+            swingValue = 17 + (rand() % 9);
+            break;
+    }
+    return swingValue;
+}
+
+int wedge(void){
+    int userNumber;
+    int swingValue;
+    const char *wedge[] = {
+    "Specify the strength of your swing:\n",
+    "1 --> Light (3 - 13yd)\n",
+    "2 -- > Medium (14 - 24yd)\n",
+    "3 --> Hard (25 - 35yd)\n"
+    };
+
+    int wedgeStrength = sizeof(wedge) / sizeof(wedge[0]);
+    for(int i = 0; i < wedgeStrength; i++){
+        printf("%s\n", wedge[i]);
+    }
+    
+    do{
+        printf("\nEnter one of the numbers listed\nYour number: ");
+        scanf("%d", &userNumber);
+    }while(userNumber < 1 || userNumber > 3);
+
+    switch(userNumber){
+        case 1:
+            swingValue = 3 + (rand() % 11);
+            break;
+
+        case 2:
+            swingValue = 14 + (rand() % 11);
+            break;
+
+        case 3:
+            swingValue = 25 + (rand() % 11);
+            break;
+    }
+    return swingValue;
+}
+
+int nineIron(void){
+    int userNumber;
+    int swingValue;
+    const char *nineIron[] = {
+        "Specify the strength of your swing:\n",
+        "1 --> Light (3 - 13yd)\n",
+        "2 -- > Medium (14 - 24yd)\n",
+        "3 --> Hard (25 - 35yd)\n"
+    };
+
+    int nineIronStrength = sizeof(nineIron) / sizeof(nineIron[0]);
+    for(int i = 0; i < nineIronStrength; i++){
+        printf("%s\n", nineIron[i]);
+    }
+    
+    do{
+        printf("\nEnter one of the numbers listed\nYour number: ");
+        scanf("%d", &userNumber);
+    }while(userNumber < 1 || userNumber > 3);
+
+    switch(userNumber){
+        case 1:
+            swingValue = 35 + (rand() % 13);
+            break;
+
+        case 2:
+            swingValue = 47 + (rand() % 13);
+            break;
+
+        case 3:
+            swingValue = 59 + (rand() % 13);
+            break;
+    }
+    return swingValue;
+}
+
+int sevenIron(void){
+    int userNumber;
+    int swingValue;
+    const char *sevenIron[] = {
+        "Specify the strength of your swing:\n",
+        "1 --> Light (72 - 84yd)\n",
+        "2 -- > Medium (84 - 96yd)\n",
+        "3 --> Hard (96 - 108yd)\n"
+    };
+
+    int sevenIronStrength = sizeof(sevenIron) / sizeof(sevenIron[0]);
+    for(int i = 0; i < sevenIronStrength; i++){
+        printf("%s\n", sevenIron[i]);
+    }
+
+    do{
+        printf("\nEnter one of the numbers listed\nYour number: ");
+        scanf("%d", &userNumber);
+    }while(userNumber < 1 || userNumber > 3);
+
+    switch(userNumber){
+        case 1:
+            swingValue = 72 + (rand() % 13);
+            break;
+
+        case 2:
+            swingValue = 84 + (rand() % 13);
+            break;
+
+        case 3:
+            swingValue = 96 + (rand() % 13);
+            break;
+    }
+    return swingValue;
+}
+
+int fiveIron(void){
+    int userNumber;
+    int swingValue;
+    const char *fiveIron[] = {
+        "Specify the strength of your swing:\n",
+        "1 --> Light (109 - 121yd)\n",
+        "2 -- > Medium (121 - 133yd)\n",
+        "3 --> Hard (133 - 145yd)\n"
+    };
+
+    int fiveIronStrength = sizeof(fiveIron) / sizeof(fiveIron[0]);
+    for(int i = 0; i < fiveIronStrength; i++){
+        printf("%s\n", fiveIron[i]);
+    }
+    
+    do{
+        printf("\nEnter one of the numbers listed\nYour number: ");
+        scanf("%d", &userNumber);
+    }while(userNumber < 1 || userNumber > 3);
+
+    switch(userNumber){
+        case 1:
+            swingValue = 109 + (rand() % 13);
+            break;
+
+        case 2:
+            swingValue = 121 + (rand() % 13);
+            break;
+
+        case 3:
+            swingValue = 133 + (rand() % 13);
+            break;
+    }
+    return swingValue;
+}
+
+int threeIron(void){
+    int userNumber;
+    int swingValue;
+    const char *threeIron[] = {
+        "Specify the strength of your swing:\n",
+        "1 --> Light (146 - 158yd)\n",
+        "2 -- > Medium (158 - 170yd)\n",
+        "3 --> Hard (170 - 182yd)\n"
+    };
+
+    int threeIronStrength = sizeof(threeIron) / sizeof(threeIron[0]);
+    for(int i = 0; i < threeIronStrength; i++){
+        printf("%s\n", threeIron[i]);
+    }
+    
+    do{
+        printf("\nEnter one of the numbers listed\nYour number: ");
+        scanf("%d", &userNumber);
+    }while(userNumber < 1 || userNumber > 3);
+
+    switch(userNumber){
+        case 1:
+            swingValue = 146 + (rand() % 13);
+            break;
+
+        case 2:
+            swingValue = 158 + (rand() % 13);
+            break;
+
+        case 3:
+            swingValue = 170 + (rand() % 13);
+            break;
+    }
+    return swingValue;
+}
+
+int threeWood(void){
+    int userNumber;
+    int swingValue;
+    const char *threeWood[] = {
+        "Specify the strength of your swing:\n",
+        "1 --> Light (183 - 195yd)\n",
+        "2 -- > Medium (195 - 207yd)\n",
+        "3 --> Hard (207 - 219yd)\n"
+    };
+
+    int threeWoodStrength = sizeof(threeWood) / sizeof(threeWood[0]);
+    for(int i = 0; i < threeWoodStrength; i++){
+        printf("%s\n", threeWood[i]);
+    }
+    
+    do{
+        printf("\nEnter one of the numbers listed\nYour number: ");
+        scanf("%d", &userNumber);
+    }while(userNumber < 1 || userNumber > 3);
+
+    switch(userNumber){
+        case 1:
+            swingValue = 183 + (rand() % 13);
+            break;
+
+        case 2:
+            swingValue = 195 + (rand() % 13);
+            break;
+
+        case 3:
+            swingValue = 207 + (rand() % 13);
+            break;
+    }
+    return swingValue;
+}
+
+int driver(void){
+    int userNumber;
+    int swingValue;
+    const char *driver[] = {
+        "Specify the strength of your swing:\n",
+        "1 --> Light (220 - 232yd)\n",
+        "2 -- > Medium (232 - 244yd)\n",
+        "3 --> Hard (244 - 255y)"
+    };
+
+    int driverStrength = sizeof(driver) / sizeof(driver[0]);
+    for(int i = 0; i < driverStrength; i++){
+        printf("%s\n", driver[i]);
+    }
+    
+    do{
+        printf("\nEnter one of the numbers listed\nYour number: ");
+        scanf("%d", &userNumber);
+    }while(userNumber < 1 || userNumber > 3);
+
+    switch(userNumber){
+        case 1:
+            swingValue = 220 + (rand() % 13);
+            break;
+
+        case 2:
+            swingValue = 232 + (rand() % 13);
+            break;
+
+        case 3:
+            swingValue = 244 + (rand() % 12);
+            break;
+    }
+    return swingValue;
+}
---
+++ golfSimulator.c
@@ -0,0 +1,202 @@
+//Header Files
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include "golfClubs.h"
+
+//Macros used to alter the color of text
+#define COLOR_BOLD "\e[1m"
+#define COLOR_OFF "\e[m"
+
+//Global Variables
+int swingsTaken = 0;
+int distanceTraveledByBall = 0;
+
+//Function Prototypes
+bool holes(int swingDistance, int holeLocation, int par);
+void distanceTraveled(int swingDistance, int holeLocation);
+bool inHole(int holeLocation, int par);
+bool maxStrokes(int par);
+void getScore(int par);
+void finalScore(int courseScore, bool print);
+void resetGlobalVariables(void);
+
+//Main Function
+int main(void){
+    int userChoice, swingDistance = 0; 
+    int holeNumber = 1;
+    int currentParOfHole = 3;
+    bool inHole = false;
+    
+    //An array of function pointer; the functions are the golf clubs
+    int (*ptrClubs[8])(void) = {putter, wedge, nineIron, sevenIron, fiveIron, threeIron, threeWood, driver};
+    //Randomizes the seeds that are used to select random values for the ranges of each golf club
+    srand(time(NULL)); 
+
+    printf(COLOR_BOLD "Welcome to Golf Simulator for beginners!\n\n" COLOR_OFF);
+    holesInfo();
+    printf(COLOR_BOLD "\nNote: This information will be displayed again upon completion of each hole!\n" COLOR_OFF);
+
+    //Loops through each hole
+    while(holeNumber < 5){
+        //Checks to see if user went over the max amount of strokes within the main function
+        if(maxStrokes(currentParOfHole) && !inHole){
+            holeNumber++;
+            resetGlobalVariables();
+
+            if(holeNumber == 5){
+                break;
+            }
+        }
+
+        //Asks the user to pick a club
+        if(!inHole){
+            clubsInfo();
+            do{
+                printf("\nPlease input the corresponding number for the club you want to select: ");
+                scanf("%d", &userChoice);
+            }while(userChoice < 0 || userChoice > 7);
+            puts("");
+            swingDistance = (*ptrClubs[userChoice])();
+        }
+
+        if(!inHole){ //Passes the information of the current hole to functions while the ball is not in the hole
+            switch(holeNumber){
+            case 1:
+                swingsTaken++;
+                inHole = holes(swingDistance, 300, currentParOfHole);
+                break;
+
+            case 2:
+                swingsTaken++;
+                currentParOfHole = 4;
+                inHole = holes(swingDistance, 500, currentParOfHole);  
+                break;
+
+            case 3:
+                swingsTaken++;
+                currentParOfHole = 3;
+                inHole = holes(swingDistance, 200, currentParOfHole);  
+                break;
+
+            case 4:
+                swingsTaken++;
+                currentParOfHole = 5;
+                inHole = holes(swingDistance, 700, currentParOfHole);  
+                break;
+            }
+        }
+        else if(inHole){ //If the ball is in the hole, changes the hole number and par to the next one, as well as resetting the global variables
+            switch(holeNumber){
+            case 1:
+                holeNumber = 2;
+                inHole = false;
+                resetGlobalVariables();
+                holesInfo();
+                puts("");
+                break;
+                
+            case 2:
+                holeNumber = 3;
+                inHole = false;
+                resetGlobalVariables();
+                holesInfo();
+                puts("");
+                break;
+
+            case 3:
+                holeNumber = 4;
+                inHole = false;
+                resetGlobalVariables();
+                holesInfo();
+                puts("");
+                break;
+
+            case 4:
+                holeNumber = 5;
+                break;
+            }
+        }
+    }
+    finalScore(0, true); //0 is passed to not add anything to the final score, and true is passed to signify the end of the game
+    printf("Thanks for playing!\n");
+    return 0;
+}
+
+//Prints the stroke number and the distance the ball traveled due to the swing for each hole. Returns boolean value to the main function to see if it is in the hole
+bool holes(int swingDistance, int holeLocation, int par){
+    printf(COLOR_BOLD"\nStroke: %d\nThe golf ball traveled %d yards\n", swingsTaken, swingDistance); 
+    printf(COLOR_OFF);
+    distanceTraveled(swingDistance, holeLocation);
+    return inHole(holeLocation, par);
+}
+
+//Obtains the total distance the ball has traveled in relation to the hole position
+void distanceTraveled(int swingDistance, int holeLocation){
+    if(distanceTraveledByBall < holeLocation){
+        distanceTraveledByBall += swingDistance;
+    }
+
+    else{
+        distanceTraveledByBall -= swingDistance;
+    }
+}
+
+//Determines if the ball is in the hole. If not, then how far the ball is from the hole
+bool inHole(int holeLocation, int par){    
+    if(distanceTraveledByBall < holeLocation){ 
+        printf(COLOR_BOLD "You are %d yard(s) away from the hole\n", holeLocation - distanceTraveledByBall);
+        printf(COLOR_OFF);
+        return false;
+    }
+
+    if(distanceTraveledByBall > holeLocation){
+        printf(COLOR_BOLD "You are %d yard(s) away from the hole\n", distanceTraveledByBall - holeLocation);
+        printf(COLOR_OFF);
+        return false;
+    }
+
+    if(distanceTraveledByBall == holeLocation){
+        printf(COLOR_BOLD "\nThe golf ball went into the hole!\n");
+        printf("Good job!\n\n" COLOR_OFF);
+        getScore(par);
+        return true;
+    }
+    return false;
+}
+
+//Checks if the user has swung too many times for the current hole. If so, obtains and prints the score for the current hole
+bool maxStrokes(int par){
+    if(swingsTaken == (par + 5)){
+        getScore(par);
+        printf(COLOR_BOLD "YOU HAVE REACHED MAX STROKES ON THIS HOLE.\nBEGINNING NEXT HOLE:\n\n\n" COLOR_OFF);
+        return true;
+    }
+    return false;
+}
+
+//Obtains and prints the score for the current hole
+void getScore(int par){
+    int courseScore = swingsTaken - par;
+    printf(COLOR_BOLD "Your score for this course is %d\n\n", courseScore);
+    printf(COLOR_OFF);
+    finalScore(courseScore, false);
+}
+
+//Sums the scores for each course to print the final score
+void finalScore(int courseScore, bool print){
+    static int finalScore;
+    finalScore += courseScore;
+
+    if(print){
+        printf(COLOR_BOLD "Your final score is: %d\n", finalScore);
+        printf(COLOR_OFF);
+    }
+}
+
+//Helper function for the global variables
+void resetGlobalVariables(void){ 
+    swingsTaken = 0;
+    distanceTraveledByBall = 0;
+}
